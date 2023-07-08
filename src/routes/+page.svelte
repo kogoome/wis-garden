d8f05fe30cd06a1c7d286654f7f0f1c36c05bd32
wistaria <kogoome@kakao.com>
2023-07-09 06:24:56 +0900

    add base path

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script>
+	import { base } from '$app/paths';
 </script>
 
 <h1 class="bg-slate-400 rounded-lg px-2">Welcome to SvelteKit</h1>
 <div class="text-emerald-300 text-lg font-bold">
-	<a href="/wis-garden/codemirror">codemirror</a>
+	<a href="{base}/codemirror">codemirror</a>
 </div>
