ea485a71de58abb34076b13945752ec6133971ba
wistaria <kogoome@kakao.com>
2023-07-08 23:42:07 +0900

    update tailwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,5 @@
   
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1 class="bg-slate-400 rounded-lg px-2">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
