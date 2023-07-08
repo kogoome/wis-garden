331deead9c33eb3349f9026f82dd9420136da82e
wistaria <kogoome@kakao.com>
2023-07-08 23:48:24 +0900

    add test router

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,4 +3,6 @@
 </script>
 
 <h1 class="bg-slate-400 rounded-lg px-2">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="text-emerald-300 text-lg font-bold"><a href="/codemirror">codemirror</a></div>
+
+
