e8b0bc7516600544a7438618a76315b6ab350cad
wistaria <kogoome@kakao.com>
2023-07-09 07:22:10 +0900

    feat codemirror decoration

--- src/routes/codemirror/+page.svelte
+++ src/routes/codemirror/+page.svelte
@@ -1,12 +1,47 @@
 <script lang="ts">
-	import { EditorView, keymap } from '@codemirror/view';
+	import { EditorView, keymap, Decoration, type DecorationSet } from '@codemirror/view';
 	import { onMount } from 'svelte';
 	import { basicSetup } from 'codemirror';
-	import { EditorState, Compartment } from '@codemirror/state';
+	import { EditorState, Compartment, StateField, StateEffect } from '@codemirror/state';
 	import { defaultKeymap } from '@codemirror/commands';
 
 	onMount(() => {
 		let tabSize = new Compartment();
+
+		const addUnderline = StateEffect.define<{ from: number; to: number }>({
+			map: ({ from, to }, change) => ({ from: change.mapPos(from), to: change.mapPos(to) })
+		});
+
+		const underlineMark = Decoration.mark({ class: 'cm-underline' });
+
+		const underlineField = StateField.define<DecorationSet>({
+			create() {
+				return Decoration.none;
+			},
+			update(underlines, tr) {
+				underlines = underlines.map(tr.changes);
+				for (let e of tr.effects)
+					if (e.is(addUnderline)) {
+						underlines = underlines.update({
+							add: [underlineMark.range(e.value.from, e.value.to)]
+						});
+					}
+				return underlines;
+			},
+			provide: (f) => EditorView.decorations.from(f)
+		});
+
+		export function underlineSelection(view: EditorView) {
+			let effects: StateEffect<unknown>[] = view.state.selection.ranges
+				.filter((r) => !r.empty)
+				.map(({ from, to }) => addUnderline.of({ from, to }));
+			if (!effects.length) return false;
+
+			if (!view.state.field(underlineField, false))
+				effects.push(StateEffect.appendConfig.of([underlineField]));
+			view.dispatch({ effects });
+			return true;
+		}
 
 		let state = EditorState.create({
 			doc: 'Hello World',
@@ -47,3 +82,9 @@
 
 <div class="text-2xl">hello codemirror!!</div>
 <div id="container" class="p-10 rounded-2xl" />
+
+<style>
+	:global(.cm-underline) {
+		text-decoration: 'underline 3px red';
+	}
+</style>
