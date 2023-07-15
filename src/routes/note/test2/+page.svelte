39f416574238542b8a1b59282aeab5ea8a1cebbe
wistaria <kogoome@kakao.com>
2023-07-16 01:05:32 +0900

    fix layout component test

---
+++ src/routes/note/test2/+page.svelte
@@ -0,0 +1,11 @@
+<script lang='ts'>
+  import NoteLayout from "$lib/layout/NoteLayout.svelte";
+</script>
+
+<NoteLayout title="hello" subtitle="world" date="2002">
+
+  <h1>hello world</h1>
+  test github page
+  이건 오류가 날 이유가 없는 컴포넌트임
+
+</NoteLayout>
