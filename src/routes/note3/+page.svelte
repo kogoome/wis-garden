d4c4b31bcb21b1ff652b3c792790e8357d80eb0e
wistaria <kogoome@kakao.com>
2023-07-16 21:40:51 +0900

    add note3 - sidebar base move +page

--- src/routes/note3/+page.svelte
+++ src/routes/note3/+page.svelte
@@ -1,8 +1,16 @@
+<script lang="ts">
+</script>
 
+<svelte:head>
+	<title>welcome wis garden</title>
+</svelte:head>
 
-<div class="w-full h-full">
-
-testPage
-
-</div>
+<article class="bg-black w-full h-screen overflow-y-scroll px-10">
+	<div class="bg-black w-full h-[80vh] text-7xl text-end flex flex-col justify-center">
+		<div class="text-gradient font-bold text-white">welcome!</div>
+	</div>
+	<div class="text-slate-200">
+		<slot />
+	</div>
+</article>
 
