02025002f24a97655a92847d0e71a529eba9f9f7
wistaria <kogoome@kakao.com>
2023-07-17 04:42:11 +0900

    main with nextra

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,24 +1,23 @@
-import React from "react";
 import { Head } from "./components/Head";
 import { useNextSeoProps } from "./config/useNextSeoProps";
 import { Logo } from "./components/Logo";
 
 const config = {
-  logo: Logo,
-  // project: {
-  //   link: 'empty'
-  // },
-  // docsRepositoryBase: 'empty',
-  footer: {
-    text: "made by wis"
-  },
-  head: Head,
-  feedback: {
-    content: () => <>Question? Give me feedback →</>,
-    labels: "feedback",
-  },
-  useNextSeoProps,
-  i18n: [],
+	logo: Logo,
+	// project: {
+	//   link: 'empty'
+	// },
+	docsRepositoryBase: 'https://github.com/path-of-reason/wis-garden',
+	footer: {
+		text: 'made by wis',
+	},
+	head: Head,
+	feedback: {
+		content: () => <>Question? Give me feedback →</>,
+		labels: 'feedback',
+	},
+	useNextSeoProps,
+	i18n: [],
 }
 
 export default config;
